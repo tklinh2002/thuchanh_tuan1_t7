695eb6eef43871271b769694f7a766670b3b3b58
Tran Khanh Linh <ktpmlinh2002@gmail.com>
2023-06-17 16:53:46 +0700

    giao dien nhan benh

---
+++ demo/src/main/java/com/example/demo/NhanBenhController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class NhanBenhController {
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private TextField txtCCCD;
+
+    @FXML
+    private TextArea txtDiaChi;
+
+    @FXML
+    private TextField txtMa;
+
+    @FXML
+    private TextField txtTen;
+
+}
